3824ea87dd2eddc04017f561165f1fd502a923b1
dhrsharma <dhruv.sharma1@aexp.com>
2016-12-24 14:28:14 -0700

    Adding install script for cassandra.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mix
+
+Created this repo to store my random work, like easy install scripts or some useful piece of code.
+
+- For now, installscripts folder has a script to install cassandra on macOS.
+- Specify cassandra version with the script like `<script> 2.2.8`.
+- This script will also modify the `~/.bash_profile` to point to newly installed cassandra.
+- Just close and reopen your terminal session to reload `~/.bash_profile` changes.
+- And now you cas run cassandra from anywhere with `cassandra -f` that will start cassandra in foreground.
---
+++ installscripts/cassandra-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+java -version 2>&1 >/dev/null | grep -q "java version"
+
+if [ $? -ne 0 ]
+then
+  echo *******Please install java 1.7 or later************
+  exit 1
+fi
+
+VERSION=$1
+
+if [ -z $VERSION ]
+then
+  echo Please provide a valid cassandra version to install. like 2.2.8
+  exit 1
+fi
+
+CASS=~/opt/cassandra
+CASS_YML=CASS/conf/cassandra.yml
+
+
+mkdir -p ~/opt/packages && cd $_ && \
+curl -O "http://apache.mirrors.ionfish.org/cassandra/$VERSION/apache-cassandra-$VERSION-bin.tar.gz" && \
+tar -xzvf apache-cassandra-$VERSION-bin.tar.gz && \
+ln -s ~/opt/packages/apache-cassandra-$VERSION/ $CASS && \
+mkdir -p $CASS/data/data && \
+mkdir -p $CASS/data/commitlog && \
+mkdir -p $CASS/data/saved_caches && \
+mkdir -p $CASS/logs && \
+brew install gnu-sed && \
+alias sed=gsed && \
+gsed -i -e '/^export PATH=/ s/$/:\~\/opt\/cassandra\/bin/' -e "$ a export CASSANDRA_HOME=$CASS" ~/.bash_profile && \
+source ~/.bash_profile
+
+
