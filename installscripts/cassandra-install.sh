4a756d53cb4aa677ed74c50fffb9694197dc591f
Dhruv Sharma <dhruvsharma.sun@gmail.com>
2017-01-04 23:01:59 -0700

    change for file edit

--- installscripts/cassandra-install.sh
+++ installscripts/cassandra-install.sh
@@ -28,11 +28,6 @@ mkdir -p $CASS/data/commitlog && \
 mkdir -p $CASS/data/saved_caches && \
 mkdir -p $CASS/logs && \
 brew install gnu-sed && \
-alias sed=gsed
-
-if [ -w ~/.bash_profile ]
-then
-  gsed -i -e '/^export PATH=/ s/$/:\~\/opt\/cassandra\/bin/' -e "$ a export CASSANDRA_HOME=$CASS" ~/.bash_profile
-fi
-
+alias sed=gsed && \
+gsed -i -e '/^export PATH=/ s/$/:\~\/opt\/cassandra\/bin/' -e "$ a export CASSANDRA_HOME=$CASS" ~/.bash_profile && \
 echo ------CASSANDRA $VERSION installed successfully.---------- 
