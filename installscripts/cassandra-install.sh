bf3233183b1d3cb1f6dbed6ed1b36db5063d530c
dhrsharma <dhruv.sharma1@aexp.com>
2017-01-05 11:40:27 -0700

    proxy changes

--- installscripts/cassandra-install.sh
+++ installscripts/cassandra-install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-java -version 2>&1 >/dev/null | grep -q "java version"
+read -p 'Please enter your ADS Id: ' username
+read -sp 'Please enter your ADS password: ' password
 
-if [ $? -ne 0 ]
-then
-  echo *******Please install java 1.7 or later************
-  exit 1
-fi
+export http_proxy=http://$username:$password@proxy-phoenix.aexp.com:8080
+export https_proxy=http://$username:$password@proxy-phoenix.aexp.com:8080
+echo .
+echo .
+echo .
+echo .
+
+#java -version 2>&1 >/dev/null | grep -q "java version"
+
+#if [ $? -ne 0 ]
+#then
+echo *******Please have java 1.7 or later installed to run cassandra after this installation************
+#  exit 1
+#fi
 
 VERSION=$1
 
@@ -29,5 +39,9 @@ mkdir -p $CASS/data/saved_caches && \
 mkdir -p $CASS/logs && \
 brew install gnu-sed && \
 alias sed=gsed && \
-gsed -i -e '/^export PATH=/ s/$/:\~\/opt\/cassandra\/bin/' -e "$ a export CASSANDRA_HOME=$CASS" ~/.bash_profile && \
+gsed -i -e '/^export PATH=/ s/$/:\~\/opt\/cassandra\/bin/' -e "$ a export CASSANDRA_HOME=$CASS" -e '$ a export PATH=$PATH:\~\/opt\/cassandra\/bin/' ~/.bash_profile && \
 echo ------CASSANDRA $VERSION installed successfully.---------- 
+
+unset http_proxy https_proxy
+
+echo Now open another shell session and hit 'cassandra -f' to run cassandra in foreground.
